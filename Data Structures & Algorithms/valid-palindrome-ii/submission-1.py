6adea0776544372f0a7e23000ff3ce6f60891793
Tulluri Krishna Sainath <krishnasainath1997@gmail.com>
2026-09-16 22:51:49 +0530

    Add: valid-palindrome-ii - submission-1

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        skipped = False
+        while (l < r):
+            if (s[l] == s[r]):
+                l += 1
+                r -= 1
+            elif not skipped: #and (r - l) > 1:
+                if (s[l+1] == s[r]):
+                    l += 1
+                    skipped = True
+                elif (s[l] == s[r-1]):
+                    r -= 1
+                    skipped = True
+                else:
+                    return False
+            else:
+                return False
+        return True
