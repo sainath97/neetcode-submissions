0a06aa6b24314b0c39c7950a23e160e9aece58c1
Tulluri Krishna Sainath <krishnasainath1997@gmail.com>
2026-09-22 16:07:14 +0530

    Add: count-primes - submission-0

---
+++ Data Structures & Algorithms/count-primes/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        count = 0
+        for i in range(2, n+1):
+            if self.isPrime(i):
+                count += 1
+        return count
+
+    def isPrime(self, n):
+        if n <= 1:
+            return False
+        elif n == 2:
+            return True
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
